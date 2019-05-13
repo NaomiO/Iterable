321315a8c5fcb89f4d66dc30a8966d9f40d53267
NaomiO <44748207+NaomiO@users.noreply.github.com>
2019-05-13 15:33:38 +0300

    Update chain.hpp
    last

--- chain.hpp
+++ chain.hpp
@@ -4,7 +4,7 @@ namespace itertools
 
 template <class A, class B>
 
-class _chain
+class chain
 {
 private:
     A first;   
@@ -22,7 +22,7 @@ private:
 
         iterator(U first, V second) : itr1(first), itr2(second), flag(true) {}
 
-        bool operator!=(_chain::iterator<U,V> const &other) 
+        bool operator!=(chain::iterator<U,V> const &other) 
         {
             if (flag && !(itr1 != (other.itr1)))
                 flag = false;
@@ -43,7 +43,7 @@ private:
                 return *itr2;
         }
 
-        _chain::iterator<U,V> &operator++()
+        chain::iterator<U,V> &operator++()
         {
             if(flag)
                 ++itr1;
@@ -55,20 +55,20 @@ private:
     };
 
 public:
-    _chain(A from, B to) : first(from), second(to) {} // constructor
+    chain(A from, B to) : first(from), second(to) {} // constructor
 
     auto begin() const{ 
-        return  _chain::iterator<decltype(first.begin()),decltype(second.begin())>(first.begin(), second.begin()); }  // iteratable object
+        return  chain::iterator<decltype(first.begin()),decltype(second.begin())>(first.begin(), second.begin()); }  // iteratable object
 
     auto end() const {
-        return _chain::iterator<decltype(first.end()),decltype(second.end())>(first.end(), second.end()); }  // iteratable object  
+        return chain::iterator<decltype(first.end()),decltype(second.end())>(first.end(), second.end()); }  // iteratable object  
 };  // class
 
 template <typename A, typename B>
 
-_chain<A, B> chain(A chain1, B chain2)
+chain<A, B> mychain(A chain1, B chain2)
 {
-    return _chain<A, B>(chain1, chain2);
+    return mychain<A, B>(chain1, chain2);
 }
 
 } 
