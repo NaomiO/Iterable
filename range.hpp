dc50a0b5d848d6f385767dc5ff4b19bd22becad5
NaomiO <naomioyer95@gmail.com>
2019-05-13 14:37:36 +0300

    commit

--- range.hpp
+++ range.hpp
@@ -1,35 +1,24 @@
 namespace itertools
 {
 
-
 template <class T>
-/*
-* _range class, using two values to iterate between them.
-* the values must be a primitive type or an object, if its an object
-* then it must implement the following operators: != (not equal), ++ (prefix increment).
-* the _range class contains inner iterator.
-* Note: calling the range(T,T) function instead of the class _range is recommended.
-*/
+
 class _range
 {
 
-// Private variables and methods
-private:
-    T _from; // starting point
-    T _to;   // stopping point.
 
-    // Inner class (iterator)
+private:
+    T start; 
+    T end;   
+
     class iterator
     {
 
     public:
-        // variables
         T val;
 
-        //constructor
-        iterator(T vall) : val(vall){}
+        iterator(T it_val) : val(it_val){}
 
-        // operators
         bool operator!=(_range::iterator const &other) const
         { 
             return val != (other.val);
@@ -43,27 +32,20 @@ private:
 
         _range::iterator &operator++()
         {
-
             ++val;
             return *this;
         }
     };
 
 public:
-    _range(T from, T to) : _from(from), _to(to) {}                      // constructor
-    _range::iterator begin() const { return _range::iterator(_from); }  // iteratable object
-    _range::iterator end() const { return _range::iterator(_to); }      // iteratable object
-}; // class
-
+    _range(T from, T to) : start(from), end(to) {}                      
+    _range::iterator begin() const { return _range::iterator(start); }  
+    _range::iterator end() const { return _range::iterator(end); }     
+};
 
 
 template <typename T>
-/*
-* range function, use in loops to iterate between two values.
-* Example use case: 
-* for(int i : range(1,5))
-*   // do somethin...
-*/
+
 _range<T> range(T from, T to)
 {
     return _range<T>(from, to);
